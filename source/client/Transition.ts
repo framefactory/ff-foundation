664712da95c9f65c2eb335942f52fd86df24e580
Ralph Wiedemeier <ralph@framefactory.ch>
2019-04-29 07:38:47 +0200

    library updates, transitions

--- source/client/Transition.ts
+++ source/client/Transition.ts
@@ -5,7 +5,11 @@
  * License: MIT
  */
 
-import CustomElement, { customElement, html } from "@ff/ui/CustomElement";
+import CustomElement, { customElement, property, html, PropertyValues } from "@ff/ui/CustomElement";
+
+import "@ff/ui/Button";
+import "@ff/ui/Transition";
+import "@ff/ui/SlideTransition";
 
 import "./styles.scss";
 
@@ -14,8 +18,48 @@ import "./styles.scss";
 @customElement("ff-foundation-transition")
 export class Application extends CustomElement
 {
-    render()
-    {
+    protected visible = true;
+    protected counter = 0;
 
+    protected render()
+    {
+        //            <ff-transition class="ff-fade-transition" ?in=${this.visible} .content=${html`<ff-test counter=${this.counter}></ff-test>`}></ff-transition>
+
+        return html`<h1>Transition</h1>
+            <ff-button text="Add" @click=${this.onClickAdd}></ff-button>
+            <ff-button text="Remove" @click=${this.onClickRemove}></ff-button>
+            <ff-button text="Increment" @click=${this.onClickIncrement}></ff-button>
+            <ff-slide-transition ?in=${this.visible} .content=${html`<ff-test counter=${this.counter}></ff-test>`}></ff-slide-transition>
+        `;
+    }
+
+    protected onClickAdd()
+    {
+        this.visible = true;
+        this.requestUpdate();
+    }
+
+    protected onClickRemove()
+    {
+        this.visible = false;
+        this.requestUpdate();
+    }
+
+    protected onClickIncrement()
+    {
+        this.counter++;
+        this.requestUpdate();
+    }
+}
+
+@customElement("ff-test")
+export class Test extends CustomElement
+{
+    @property({ type: Number })
+    counter = 0;
+
+    protected render()
+    {
+        return html`Test Counter: ${this.counter}`;
     }
 }
